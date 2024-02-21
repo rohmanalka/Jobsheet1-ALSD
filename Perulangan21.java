d09225c21e8d7050d01853e6f38a8fea614faec1
rohmanalka <rohmanalka06@gmail.com>
2024-02-21 17:01:30 +0700

    perulangan

---
+++ Perulangan21.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Perulangan21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan digit akhir NIM Anda: ");
+        int nim = scanner.nextInt();
+
+        if (nim < 10) {
+            nim += 10;
+        }
+
+        for (int i = 1; i <= nim; i++) {
+            if (i == 6 || i == 10) {
+                continue;
+            }
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            } else {
+                System.out.print("* ");
+            }
+        }
+    }
+}
